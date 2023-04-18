effc5285a5df168061d57ba81658dc73ebf2b4c7
Triyonorifan11 <triyonorifan01@gmail.com>
2023-04-18 07:19:54 +0700

    update configurasi projek dengan webpack bundle

--- readme.md
+++ readme.md
@@ -3,3 +3,11 @@
 ### Kelompok 6
 - Triyono Rifan
 - Moh Dodi Setyawan
+
+### Step installation
+1. clone or download this repository
+2. run terminal / new terminal in vs code or git bash
+3. please type "npm intall" in terminal
+4. please wait until finish installation
+5. and type "npm run dev" in terminal
+6. so, you can see projek on http://localhost:9000/
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,19 @@
-console.log('index')
+import 'regenerator-runtime';
+import '../styles/style.css';
+import App from './views/app';
+
+const app = new App({
+  header: document.querySelector('#header'),
+  aside: document.querySelector('#aside'),
+  content: document.querySelector('#mainContent'),
+});
+
+window.addEventListener('hashchange', () => {
+  app.renderPage();
+});
+
+window.addEventListener('load', () => {
+  app.renderPage();
+});
+
+console.log('Hello Coders!');
