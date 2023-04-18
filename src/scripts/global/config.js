d296c8faa51e1e2373a92c43bae1356b6c15d4d0
Triyonorifan11 <triyonorifan01@gmail.com>
2023-04-18 21:09:15 +0700

    install firebase

--- src/scripts/global/config.js
+++ src/scripts/global/config.js
@@ -1,1 +1,21 @@
-console.log('untuk config');
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyCUjjWxjZwg2zD_gYoERwVVRhyCkKGW8Q0',
+  authDomain: 'collexi-fp-1-hactiv8.firebaseapp.com',
+  projectId: 'collexi-fp-1-hactiv8',
+  storageBucket: 'collexi-fp-1-hactiv8.appspot.com',
+  messagingSenderId: '277007697873',
+  appId: '1:277007697873:web:aceb38b78a7a4da3df82f0',
+  measurementId: 'G-C77V9YFBCZ',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
