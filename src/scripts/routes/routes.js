2abcc0a63bcb72d6777f78dd1889fc918a4f53c1
Dodistywn <dodisetya000@gmail.com>
2023-04-20 12:50:25 +0700

    update ruter

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -2,12 +2,16 @@ import Home from '../views/pages/home';
 import Detail from '../views/pages/detail';
 import Login from '../views/pages/login';
 import Register from '../views/pages/register';
+import Contact from '../views/pages/contact';
+import Product from '../views/pages/product';
 
 const routes = {
   '/': Home, // default page
   '/detail': Detail,
   '/login': Login,
   '/register': Register,
+  '/contact': Contact,
+  '/product': Product,
 };
 
 export default routes;
