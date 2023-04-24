b5a1670a554abb8b6848b3fa49b409f89a04132d
Dodistywn <dodisetya000@gmail.com>
2023-04-24 15:52:07 +0700

    memodifikasi dan update file  product.js, index.html, dan home.js

--- src/scripts/views/pages/product.js
+++ src/scripts/views/pages/product.js
@@ -4,7 +4,7 @@ const Product = {
     async render() {
       return `
             <div class="jumbotron jumbotron-product text-center pt-5 pb-5">
-                <h3 class="display-5">Selamat Datang di Product Kami</h3>
+                <h3 class="display-6">Selamat Datang di Product Kami</h3>
                 <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
                 <hr class="my-3">
                 <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
@@ -14,7 +14,7 @@ const Product = {
 
             <section id="product" class="product section-product bg-secondary pb-4 pt-5">
                 <div class="row mb-4">
-                    <div class="col-md">
+                    <div class="col-sm-6 col-lg-3 hovered-card mb-3">
                         <div class="card">
                             <img src="" class="card-img-top" alt="...">
                             <div class="card-body">
@@ -29,22 +29,7 @@ const Product = {
                             </div>
                         </div>
                     </div>
-                    <div class="col-md">
-                        <div class="card">
-                            <img src="" class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <a href="#" class="list-group-item list-group-item-action active">
-                                    <div class="d-flex justify-content-between">
-                                    <h5 class="mb-1">iugiuwdhyda</h5>
-                                    <small>3 days ago</small>
-                                    </div>
-                                    <p class="mb-1">Rp....</p>
-                                  <small>Lorem ipsum dolor sit amet consectetur adipisicing elit.</small>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md">
+                    <div class="col-sm-6 col-lg-3 hovered-card mb-3">
                         <div class="card">
                             <img src="" class="card-img-top" alt="...">
                             <div class="card-body">
@@ -59,7 +44,7 @@ const Product = {
                             </div>
                         </div>
                     </div>
-                    <div class="col-md">
+                    <div class="col-sm-6 col-lg-3 hovered-card mb-3">
                         <div class="card">
                             <img src="" class="card-img-top" alt="...">
                             <div class="card-body">
@@ -74,7 +59,7 @@ const Product = {
                             </div>
                         </div>
                     </div>
-                    <div class="col-md">
+                    <div class="col-sm-6 col-lg-3 hovered-card mb-3">
                         <div class="card">
                             <img src="" class="card-img-top" alt="...">
                             <div class="card-body">
@@ -89,9 +74,7 @@ const Product = {
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="row mb-4">
-                    <div class="col-md">
+                    <div class="col-sm-6 col-lg-3 hovered-card mb-3">
                         <div class="card">
                             <img src="" class="card-img-top" alt="...">
                             <div class="card-body">
@@ -106,7 +89,7 @@ const Product = {
                             </div>
                         </div>
                     </div>
-                    <div class="col-md">
+                    <div class="col-sm-6 col-lg-3 hovered-card mb-3">
                         <div class="card">
                             <img src="" class="card-img-top" alt="...">
                             <div class="card-body">
@@ -121,7 +104,7 @@ const Product = {
                             </div>
                         </div>
                     </div>
-                    <div class="col-md">
+                    <div class="col-sm-6 col-lg-3 hovered-card mb-3">
                         <div class="card">
                             <img src="" class="card-img-top" alt="...">
                             <div class="card-body">
@@ -136,7 +119,7 @@ const Product = {
                             </div>
                         </div>
                     </div>
-                    <div class="col-md">
+                    <div class="col-sm-6 col-lg-3 hovered-card mb-3">
                         <div class="card">
                             <img src="" class="card-img-top" alt="...">
                             <div class="card-body">
@@ -151,7 +134,7 @@ const Product = {
                             </div>
                         </div>
                     </div>
-                    <div class="col-md">
+                    <div class="col-sm-6 col-lg-3 hovered-card mb-3">
                         <div class="card">
                             <img src="" class="card-img-top" alt="...">
                             <div class="card-body">
@@ -166,9 +149,7 @@ const Product = {
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="row mb-4">
-                    <div class="col-md">
+                    <div class="col-sm-6 col-lg-3 hovered-card mb-3">
                         <div class="card">
                             <img src="" class="card-img-top" alt="...">
                             <div class="card-body">
@@ -183,7 +164,7 @@ const Product = {
                             </div>
                         </div>
                     </div>
-                    <div class="col-md">
+                    <div class="col-sm-6 col-lg-3 hovered-card mb-3">
                         <div class="card">
                             <img src="" class="card-img-top" alt="...">
                             <div class="card-body">
@@ -198,7 +179,7 @@ const Product = {
                             </div>
                         </div>
                     </div>
-                    <div class="col-md">
+                    <div class="col-sm-6 col-lg-3 hovered-card mb-3">
                         <div class="card">
                             <img src="" class="card-img-top" alt="...">
                             <div class="card-body">
@@ -213,7 +194,7 @@ const Product = {
                             </div>
                         </div>
                     </div>
-                    <div class="col-md">
+                    <div class="col-sm-6 col-lg-3 hovered-card mb-3">
                         <div class="card">
                             <img src="" class="card-img-top" alt="...">
                             <div class="card-body">
@@ -228,7 +209,7 @@ const Product = {
                             </div>
                         </div>
                     </div>
-                    <div class="col-md">
+                    <div class="col-sm-6 col-lg-3 hovered-card mb-3">
                         <div class="card">
                             <img src="" class="card-img-top" alt="...">
                             <div class="card-body">
@@ -243,9 +224,7 @@ const Product = {
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="row mb-4">
-                    <div class="col-md">
+                    <div class="col-sm-6 col-lg-3 hovered-card mb-3">
                         <div class="card">
                             <img src="" class="card-img-top" alt="...">
                             <div class="card-body">
@@ -260,7 +239,7 @@ const Product = {
                             </div>
                         </div>
                     </div>
-                    <div class="col-md">
+                    <div class="col-sm-6 col-lg-3 hovered-card mb-3">
                         <div class="card">
                             <img src="" class="card-img-top" alt="...">
                             <div class="card-body">
@@ -275,7 +254,7 @@ const Product = {
                             </div>
                         </div>
                     </div>
-                    <div class="col-md">
+                    <div class="col-sm-6 col-lg-3 hovered-card mb-3">
                         <div class="card">
                             <img src="" class="card-img-top" alt="...">
                             <div class="card-body">
@@ -290,7 +269,7 @@ const Product = {
                             </div>
                         </div>
                     </div>
-                    <div class="col-md">
+                    <div class="col-sm-6 col-lg-3 hovered-card mb-3">
                         <div class="card">
                             <img src="" class="card-img-top" alt="...">
                             <div class="card-body">
@@ -305,7 +284,7 @@ const Product = {
                             </div>
                         </div>
                     </div>
-                    <div class="col-md">
+                    <div class="col-sm-6 col-lg-3 hovered-card mb-3">
                         <div class="card">
                             <img src="" class="card-img-top" alt="...">
                             <div class="card-body">
@@ -320,9 +299,7 @@ const Product = {
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="row mb-4">
-                    <div class="col-md">
+                    <div class="col-sm-6 col-lg-3 hovered-card mb-3">
                         <div class="card">
                             <img src="" class="card-img-top" alt="...">
                             <div class="card-body">
@@ -337,7 +314,7 @@ const Product = {
                             </div>
                         </div>
                     </div>
-                    <div class="col-md">
+                    <div class="col-sm-6 col-lg-3 hovered-card mb-3">
                         <div class="card">
                             <img src="" class="card-img-top" alt="...">
                             <div class="card-body">
@@ -352,7 +329,7 @@ const Product = {
                             </div>
                         </div>
                     </div>
-                    <div class="col-md">
+                    <div class="col-sm-6 col-lg-3 hovered-card mb-3">
                         <div class="card">
                             <img src="" class="card-img-top" alt="...">
                             <div class="card-body">
@@ -367,7 +344,7 @@ const Product = {
                             </div>
                         </div>
                     </div>
-                    <div class="col-md">
+                    <div class="col-sm-6 col-lg-3 hovered-card mb-3">
                         <div class="card">
                             <img src="" class="card-img-top" alt="...">
                             <div class="card-body">
@@ -382,7 +359,7 @@ const Product = {
                             </div>
                         </div>
                     </div>
-                    <div class="col-md">
+                    <div class="col-sm-6 col-lg-3 hovered-card mb-3">
                         <div class="card">
                             <img src="" class="card-img-top" alt="...">
                             <div class="card-body">
